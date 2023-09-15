5d574ca239b81239e46e56ab053f3abb3ee20ff4
Luigi Cardone <lcardone22@gmail.com>
2023-09-15 17:15:26 +0200

    v.0.0.1 Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Navbar, Container, Col, Carousel, Image, Row } from 'react-bootstrap';
+import img_one from './assets/swarosim.png'
+import img_two from './assets/crm.png'
+import img_three from './assets/portfolio.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">La casa di Lorenzo</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Row className='px-5 py-3'>
+        <Col xs={8}>
+        <Carousel className='bg-black mx-5 my-2'>
+          <Carousel.Item interval={5000}>
+            <Image className='opacity-75 ' fluid src={img_one} />
+            <Carousel.Caption>
+              <h3>Swarosim</h3>
+              <p>Un simulatore di flora e fauna.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <Image className='opacity-75' fluid src={img_two} />
+            <Carousel.Caption>
+              <h3>CRM</h3>
+              <p>Gestionale di constratti e documenti dei clienti.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <Image className='opacity-75' fluid src={img_three} />
+            <Carousel.Caption>
+              <h3>Portfolio</h3>
+              <p>Un sito web specializzato nel mostrare il proprio lavoro.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        </Col>
+        <Col className='d-flex flex-column align-items-start' xs={4}>
+          <h1>Big text here</h1>
+          <p> Very long text here</p>
+        </Col>
+      </Row>
     </div>
   );
 }
